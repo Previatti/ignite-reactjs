f899d6bcd42654b8c436a915098b420107a81c9a
Alexandre Previatti <previatti@gmail.com>
2021-07-20 00:03:01 -0400

    usando typescript

--- 01-github-explorer/src/components/RepositoryList.tsx
+++ 01-github-explorer/src/components/RepositoryList.tsx
@@ -2,8 +2,14 @@ import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     useEffect(() => {
         fetch('https://api.github.com/users/Previatti/repos')
@@ -13,7 +19,7 @@ export function RepositoryList() {
 
     return (
         <section className="repository-list">
-            <h1>Lista de Repositorios</h1>
+            <h1>Lista de Repositorios...</h1>
 
             <ul>
                 {repositories.map(repository => (
